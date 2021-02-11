3b23c7e5ecd926f3f1fe609aaa81c633ee2ea694
Badran <mohamedbadran.yahyaoui@esprit.tn>
2021-02-11 14:42:51 +0100

    v2.0
    v2.0

--- CodeGen/TeacherFindRequest.cs
+++ CodeGen/TeacherFindRequest.cs
@@ -1,9 +1,5 @@
 public class TeacherFindRequest : FindRequest<Teacher> {
 
  
-  [JsonProperty("teachername")]
-  public  string  TeacherName { get; set; }
-  [JsonProperty("teacheradress")]
-  public  string  TeacherAdress { get; set; }
 
 }
